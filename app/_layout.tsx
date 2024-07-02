9aa21c34f4dfb2ee6614e106e2a09674dc143448
Luis-Fernando <105455585+luis13005@users.noreply.github.com>
2024-07-01 21:36:41 -0400

    primeira pagina

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,46 @@
-import { View, Text, StyleSheet, Image } from 'react-native';
-
-import ImagemTeste from '../assets/images/cloud.png'
+import { View, Text, StyleSheet, Image, StatusBar } from 'react-native';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Image src={ImagemTeste} style={styles.image}></Image>
-    </View>
+      <Image source={{ uri: 'https://i.ibb.co/YtVM9Qv/Image.png' }} style={styles.image}></Image>
+      <Image source={{ uri: 'https://i.ibb.co/PDwRhW1/temperature.png' }} style={styles.imagemiddlebot}></Image>
+      <Image source={{ uri: 'https://i.ibb.co/XymBJdV/t-EMPO-PARA-TODOS.png' }} style={styles.imagemiddletop}></Image>
+      <Image source={{ uri: 'https://i.ibb.co/7zMR8F4/15-fog-line.png' }} style={styles.imageBottom}></Image>
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#1e1e1e'
+    backgroundColor: '#1e1e1e',
+    position: 'relative'
   },
   image: {
-    marginTop: 30,
-    marginBottom: 10,
-    width: '20%',
-    height: 200,
+    position: 'absolute',
+    top: 75,
+    width: 116,
+    height: 100,
+  },
+  imagemiddletop: {
+    position: 'absolute',
+    top: 300,
+    width: 130,
+    height: 50,
+    resizeMode: 'contain'
+  },
+  imagemiddlebot: {
+    position: 'absolute',
+    top: 500,
+    width: 283.5,
+    height: 80,
+  },
+  imageBottom: {
+    position: 'absolute',
+    top: 700,
+    width: 48,
+    height: 48
   }
 });
